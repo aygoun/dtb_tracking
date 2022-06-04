041d0f0abd86462774cf1e51d341829e6063ab80
Armand B <armand.blin@gmail.com>
2022-06-04 10:17:48 +0200

    Almost done, added line graph and replacement when bad username input

--- src/components/BodyContent/BodyContent.js
+++ src/components/BodyContent/BodyContent.js
@@ -5,6 +5,14 @@ import { initializeApp } from "firebase/app";
 import { doc, getDoc, getFirestore } from "firebase/firestore";
 import MonoStackedBar from "mono-stacked-bar";
 import "mono-stacked-bar/dist/index.css";
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+} from "recharts";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -25,8 +33,10 @@ const db = getFirestore(app);
 function BodyContent() {
   const [username, setUsername] = useState("");
   const [data, setData] = useState({});
+  const [dataGraph, setDataGraph] = useState([]);
 
   const handleUsername = async () => {
+    setData({});
     console.log(username);
     const docRef = doc(db, "users", username);
     const docSnap = await getDoc(docRef);
@@ -34,10 +44,26 @@ function BodyContent() {
     if (docSnap.exists()) {
       console.log("Document data:", docSnap.data());
       setData(docSnap.data());
+      let graphdata = [];
+      //Create an array with all data.durations and data.times
+      for (let i = 0; i < docSnap.data().durations.length; i++) {
+        console.log("date: ", docSnap.data().durations[i].date);
+        console.log("time: ", docSnap.data().durations[i].time);
+        graphdata.push(
+          {
+            name: docSnap.data().durations[i].date,
+            uv: docSnap.data().durations[i].time,
+            pv: 2400,
+            amt: 2400,
+          },
+        );
+      }
+      setDataGraph(graphdata);
     } else {
       // doc.data() will be undefined in this case
       console.log("No such document!");
       setData({});
+      setDataGraph([]);
     }
   };
 
@@ -60,21 +86,41 @@ function BodyContent() {
           </button>
         </div>
       </div>
-      {data ? (
-        <div className="main-div-content">
+      {data.wins !== undefined ? (
+        <div className="main-div-content" style={{ marginTop: "3rem" }}>
           <div className="ration-win-rate">
-                <MonoStackedBar
-                data={[
-                    { value: data.wins, color: "green", caption: "Wins" },  
-                    { value: data.loses, color: "orange", caption: "LOSES" },
-                ]}
-                radius={5}
-                width={1000000000}
-                />
+            <MonoStackedBar
+              data={[
+                { value: data.wins, color: "green", caption: "Wins" },
+                { value: data.loses, color: "orange", caption: "LOSES" },
+              ]}
+              radius={5}
+              width={1000000000}
+            />
           </div>
+          <center>
+            {dataGraph.length === 0 ? (
+              <div>
+                <h1>No graph data found</h1>
+              </div>
+            ) : (
+              <>
+                <h3 style={{ marginTop: "3rem" }}>
+                  Here is your wins according to time
+                </h3>
+                <LineChart width={800} height={400} data={dataGraph}>
+                  <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+                  <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                  <XAxis dataKey="name" stroke="#8884d8" />
+                  <YAxis stroke="#8884d8" />
+                  <Tooltip />
+                </LineChart>
+              </>
+            )}
+          </center>
         </div>
       ) : (
-        <div>Unknown user!</div>
+        <h2 style={{ textAlign: "center" }}>Please enter a known username!</h2>
       )}
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,12 @@ import './Header.css';
 function Header() {
   return (
     <div className="container-header">
-        <h1>DTB | Start tracking your progress!</h1>
+      <h1>
+        <a style={{textDecoration: "none", color: "white"}} href="https://aygoun.github.io/Attack-On-Castle-Web.github.io/">
+          DTB
+        </a>{" "}
+        | Start tracking your progress!
+      </h1>
     </div>
   );
 }
